0078950036ca2156dd185416189ac9145f0afbc0
Kafi Anan Rafi <kafianan01@gmail.com>
2021-11-19 23:24:00 +0600

    2.2 - Global Object

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function sayHello(name) {
-  console.log("Hello " + name);
-}
+console.log() // global Object 
 
-sayHello('Mosh');
+setTimeout() // global Object 
+clearTimeout() // global Object 
+
+setInterval() // global Object 
+clearTimeout() // global Object 
+
+var message = '';
+
+// browser -> window represents global object
+window.console.log();
+window.setTimeout();
+
+// in Node we have global as global object
+global.console.log()
+global.setInterval()
+
+console.log(global.message) // undefined 
+
+// because Variables and Functions are scoped to that file/module 
+// thats why they are not available to the global object
