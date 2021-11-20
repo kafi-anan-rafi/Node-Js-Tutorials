d5555ccf12d068d7a4ea01cc6f4553bfc4c34197
Kafi Anan Rafi <kafianan01@gmail.com>
2021-11-20 13:13:22 +0600

    2.10 - Events Module

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const fs = require('fs') // fs module 
 
-// synchronous methods 
-var files = fs.readdirSync('./')
-console.log(files);
+// EventEmitter naming convension indicates its a - Class
+const EventEmitter = require('events');
 
-// asynchronous methods
-fs.readdir('$', function (err, files) {
-  if (err) console.log('Error', err);
-  else console.log("Result", files)
-})
+const emitter = new EventEmitter() // creating instance/object of EventEmitter class
+
+// Emit means - making a noise or procuce something
+// Emit used to raise an event
+
+// 1. Register Listener
+// emitter.addListener & emitter.on are same
+emitter.on('messageLogged', function () {
+  console.log('Listener Called!')
+});
+
+// 2. Raise Event
+emitter.emit('messageLogged');
+
+// Order is important
+// Register listener should place before Raise Event
