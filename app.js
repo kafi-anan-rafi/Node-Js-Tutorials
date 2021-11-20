0fdd0d73a004a23a9e382769ff3cff312b73aeb4
Kafi Anan Rafi <kafianan01@gmail.com>
2021-11-20 12:32:29 +0600

    2.7 - Path Module

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// loading module 
-const logger = require('./logger') // always use const data type to prevent errors
-
-// logger.log("Mosh Hamedani");   ## calling as Object 
-
-logger('Mosh Hamedani'); // calling as Function
+const path = require('path'); // path module 
+var pathObj = path.parse(__filename); // path object has some utility function like parse
+console.log(pathObj);
